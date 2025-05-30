0b0fe48183ae252b082ad05d421a9f7af43fbf45
Thalysom <thalysom.wolfgram@gmail.com>
2025-05-30 09:56:45 -0400

    criando os models

---
+++ djangoapp/site_setup/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from site_setup import models
+
+# Register your models here.
+@admin.register(models.MenuLink)
+class MenuLinkAdmin(admin.ModelAdmin):
+    list_display = 'id', 'text', 'url_or_path', 'new_tab',
+    list_display_links = 'id', 'text', 'url_or_path',
+    search_fields = 'id', 'text', 'url_or_path',
+
+
+@admin.register(models.SiteSetup)
+class SiteSetupAdmin(admin.ModelAdmin):
+    list_display = 'title', 'description'
+
+    def has_add_permission(self, request):
+        return not models.SiteSetup.objects.exists()
