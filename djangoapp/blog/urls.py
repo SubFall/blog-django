e0c02b6f47c6acd68b71569ebaa2ff66410d8afd
Thalysom <thalysom.wolfgram@gmail.com>
2025-06-10 00:41:24 -0400

    foi configurado o _post-card.html para pegar os valres de forma dinamica

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -4,7 +4,7 @@ from blog.views import index, page, post
 
 urlpatterns = [
     path('', index, name='index'),
-    path('post/', post, name='post'),
+    path('post/<slug:slug>/', post, name='post'),
     path('page/', page, name='page'),
 ]
 
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from blog.models import Post
 
-# Create your views here.
+QTDE_PASGE = 9
+
 def index(request):
+    post_list = Post.objects.filter(is_published=True).order_by('-pk')
+    paginator = Paginator(post_list, QTDE_PASGE)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     return render(
         request,
-        'blog/pages/index.html'
+        'blog/pages/index.html',
+        {'page_obj': page_obj}
     )
 
 def post(request):
