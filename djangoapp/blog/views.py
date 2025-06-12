567e450a9a06e1d0223f4d79d9da82b3a2f330a6
Thalysom <thalysom.wolfgram@gmail.com>
2025-06-12 00:12:07 -0400

    ainda falta terminar a página do post.html

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -16,10 +16,38 @@ def index(request):
         {'page_obj': page_obj}
     )
 
-def post(request):
+def created_by(request, author_pk):
+    post_list = Post.objects.get_published().filter(created_by__pk=author_pk)
+    paginator = Paginator(post_list, QTDE_PASGE)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     return render(
         request,
-        'blog/pages/post.html'
+        'blog/pages/index.html',
+        {'page_obj': page_obj}
+    )
+
+def category(request, slug):
+    post_list = Post.objects.get_published().filter(category__slug=slug)
+    paginator = Paginator(post_list, QTDE_PASGE)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(
+        request,
+        'blog/pages/index.html',
+        {'page_obj': page_obj}
+    )
+
+
+def post(request, slug):
+    post = Post.objects.filter(slug=slug).first()
+
+    return render(
+        request,
+        'blog/pages/post.html',
+        {'post': post}
     )
 
 def page(request):
